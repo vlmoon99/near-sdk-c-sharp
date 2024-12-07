389adcda56c263a134b17a00dff0e0d059d27ab2
Vladyslav <vlmoon.near@gmail.com>
2024-12-07 16:18:50 +0200

    (feature) : improve wasm compialtion

--- smartcontract/SmartContract.cs
+++ smartcontract/SmartContract.cs
@@ -2,37 +2,39 @@
 using System.Runtime.InteropServices;
 namespace smartcontract
 {
+
+    //All sys mports must be changed after compilation from "-" to "_" but for now we need to do this mannualy
     public unsafe static class WasmImports
     {
         [WasmImportLinkage]
-        [DllImport("env", EntryPoint = "storage_read")]
+        [DllImport("env", EntryPoint = "storage-read")]
         public static extern long StorageRead(long param1, long param2, long param3);
 
         [WasmImportLinkage]
-        [DllImport("env", EntryPoint = "storage_write")]
+        [DllImport("env", EntryPoint = "storage-write")]
         public static extern long StorageWrite(long param1, long param2, long param3, long param4, long param5);
 
         [WasmImportLinkage]
-        [DllImport("env", EntryPoint = "storage_has_key")]
+        [DllImport("env", EntryPoint = "storage-has_key")]
         public static extern long StorageHasKey(long param1, long param2);
 
         [WasmImportLinkage]
-        [DllImport("env", EntryPoint = "read_register")]
+        [DllImport("env", EntryPoint = "read-register")]
         public static extern void ReadRegister(long param1, long param2);
 
         [WasmImportLinkage]
-        [DllImport("env", EntryPoint = "value_return")]
+        [DllImport("env", EntryPoint = "value-return")]
         public static extern long ValueReturn(long param1, long param2);
 
         [WasmImportLinkage]
-        [DllImport("env", EntryPoint = "log_utf8")]
+        [DllImport("env", EntryPoint = "log-utf8")]
         public static extern void LogUtf8(long param1, long param2);
     }
 
     public unsafe static class SmartContract
     {
         
-        [UnmanagedCallersOnly(EntryPoint = "HelloWorld")]
+        [UnmanagedCallersOnly(EntryPoint = "helloworld")]
         public unsafe static void HelloWorld()
         {
             Log();
--- test-wasm/src/main.rs
+++ test-wasm/src/main.rs
@@ -2,12 +2,13 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let worker: near_workspaces::Worker<near_workspaces::network::Sandbox> = near_workspaces::sandbox().await?;
-    const NFT_WASM_FILEPATH: &str = "../smartcontract/test-wasm/smartcontract.wasm";
+    
+    const NFT_WASM_FILEPATH: &str = "../smartcontract/test-wasm/test.wasm";
     let wasm = std::fs::read(NFT_WASM_FILEPATH)?;
     let contract = worker.dev_deploy(&wasm).await?;
     
     let outcome = contract
-        .call("HelloWorld")
+        .call("helloworld")
         .transact()
         .await?;
 
