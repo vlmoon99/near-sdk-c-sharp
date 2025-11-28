f39e74a0462116b63103f1b6d8338bba6dd3649f
Vlad <vlmoon@proton.me>
2025-11-28 19:02:58 +0000

    (feature): add wasm case 1

---
+++ WasmCase1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Wasmtime;
+
+using var engine = new Engine();
+
+using var module = Module.FromText(
+    engine,
+    "hello",
+    "(module (func $hello (import \"\" \"hello\")) (func (export \"run\") (call $hello)))"
+);
+
+using var linker = new Linker(engine);
+using var store = new Store(engine);
+
+linker.Define(
+    "",
+    "hello",
+    Function.FromCallback(store, () => Console.WriteLine("Hello from C#!"))
+);
+
+var instance = linker.Instantiate(store, module);
+var run = instance.GetAction("run")!;
+run();
